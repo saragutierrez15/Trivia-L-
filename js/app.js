a0ce918536481351ba62b2c3c71a94c794d40e3c
saragutierrez15 <76055632+saragutierrez15@users.noreply.github.com>
2021-01-20 21:31:36 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,8 +1,3 @@
-/*const form = document.querySelector('form');
-const gname = document.getElementById('gname');
-const submit = document.getElementById('submit');
-const para = document.querySelector('p');
-*/
 
 
 function saveName(event) {
@@ -13,7 +8,7 @@ while(username==""||username==null){
   alert("Necesitas un nombre para jugar");
   username=prompt('Ingrese su nombre');
 }
-
+ 
  /* 
   
   if ( username == "" ) {
@@ -23,8 +18,6 @@ while(username==""||username==null){
     mesgElem.innerHTML =  'Necesitas un nombre para jugar!';
   }
   */
-  
-  
   // alert(username);
   localStorage.setItem('nombreUsuario', username);
   document.location.href = 'pantalla_bienvenida.html';
@@ -37,7 +30,11 @@ function bringName(){
   var nombreUsuario = localStorage.getItem('nombreUsuario');
   console.log(nameElem);
   nameElem.innerHTML +=  "Hola " + nombreUsuario + "!";
+}
 
+function bringScore(){
+  var scoreElem = document.querySelector("#mensaje-puntaje");
+  scoreElem.innerHTML =  "Tu puntaje es " + score ;
 }
 
 function redirigirCategoria(categoria){
@@ -53,56 +50,118 @@ function redirigirCategoria(categoria){
     var elem = document.getElementById('categoria2');
     var elem2 = document.getElementById('pregunta2');
     var elem3 = document.getElementById('pregunta3');
+    var elemPuntaje1 = document.getElementById('puntaje1');
     elem.style.display = 'none';
     elem2.style.display = 'none'
     elem3.style.display = 'none';
+    elemPuntaje1.style.display = 'none';
 
   } else if( categoria == 'categoria2'){
     // console.log ("Entró a categoría geografía");
     var elem = document.getElementById('categoria1');
     var elem2 = document.getElementById('pregunta5');
     var elem3 = document.getElementById('pregunta6');
+    var elemPuntaje2 = document.getElementById('puntaje2');
     elem.style.display = 'none';
     elem2.style.display = 'none'
     elem3.style.display = 'none';
+    elemPuntaje2.style.display = 'none';
   }
 }
 
+var score1 = 0;
 
-function siguientePregunta1(pregunta){
+function siguientePregunta1(pregunta, correcta){
 
   var elem = document.querySelector('#'+pregunta);
   elem.style.display = 'block';
   var elem1 = document.getElementById('pregunta1');
   var elem2 = document.getElementById('pregunta2');
   var elem3 = document.getElementById('pregunta3');
-  
+  var elemPuntaje1 = document.getElementById('puntaje1');
+
   if(pregunta == 'pregunta2'){
     elem1.style.display = 'none';
     elem3.style.display = 'none';
+    elemPuntaje1.style.display = 'none';
 
   } else if( pregunta == 'pregunta3'){
     elem1.style.display = 'none';
     elem2.style.display = 'none';
-  } 
+    elemPuntaje1.style.display = 'none';
+  }
+
+  if(correcta){
+    score1 ++;
+    // Esto permite guardar globalmente una variable
+    localStorage.setItem('score1', score1);
+    console.log('respuestas Correctas:'+score1);
+  }
+
 }
 
-function siguientePregunta2(pregunta){
+var score2 = 0;
+
+function siguientePregunta2(pregunta, correcta){
 
   var elem = document.querySelector('#'+pregunta);
   elem.style.display = 'block';
   var elem1 = document.getElementById('pregunta4');
   var elem2 = document.getElementById('pregunta5');
   var elem3 = document.getElementById('pregunta6');
+  var elemPuntaje2 = document.getElementById('puntaje2');
+
   
   if(pregunta == 'pregunta5'){
     elem1.style.display = 'none';
     elem3.style.display = 'none';
+    elemPuntaje2.style.display = 'none';
 
   } else if( pregunta == 'pregunta6'){
     elem1.style.display = 'none';
     elem2.style.display = 'none';
+    elemPuntaje2.style.display = 'none';
   } 
+
+  if(correcta){
+    score2 ++;
+    localStorage.setItem('score2', score2);
+    console.log('respuestas Correctas:'.score2);
+  }
+}
+
+function mostrarPuntaje(categoria, correcta){
+
+  var nombreUsuario = localStorage.getItem('nombreUsuario');
+  var elem3 = document.getElementById('pregunta3');
+  var elem6 = document.getElementById('pregunta6');
+  
+  if(categoria == 'categoria1'){
+    var elem = document.getElementById('puntaje1');
+    if(correcta){
+      score1 ++;
+    }
+    elem.innerHTML +=  nombreUsuario + " tu puntaje es " + score1;
+    elem.style.display = 'block';
+    elem3.style.display = 'none';
+  }
+
+  if(categoria == 'categoria2'){
+    var elem = document.getElementById('puntaje2');
+    
+    if(correcta){
+      score2 ++;
+    }
+    elem.innerHTML +=  nombreUsuario + " tu puntaje es " + score2;
+    elem.style.display = 'block';
+    elem6.style.display = 'none';
+  }
+  
+  
+  
+
+  
+  // elem.style.display = 'block';
 }
 /*
 
